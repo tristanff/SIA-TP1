6f75deb6832d4d35c2a9d45033e95dd892fca93f
albabjorkman <abjorkman@itba.edu.ar>
2024-03-22 09:48:06 -0300

    DFS - file

--- DFS.py
+++ DFS.py
@@ -1,2 +1,10 @@
-def DFS(sokoban)
+import sokoban
+import time from time
+
+def print_results
+def DFS(sokoban):
+    nodes_generated = 0
+    nodes_repeated = 0
+if sokoban.goal_state():
+    end = time()
 
