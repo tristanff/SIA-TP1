832637b1fe8f950df5a4e9f16d01f2660dc39036
albabjorkman <141673187+albabjorkman@users.noreply.github.com>
2024-03-26 09:26:22 -0300

    Add for diff os play.py
    Adding different clearing for the different operating system

--- play.py
+++ play.py
@@ -3,12 +3,20 @@ from sokoban import *
 def main():
     state = initial_state_level2
     while True:
-        os.system('clear')  # clear the console
+        if o_system == 'Windows':  # Clearing depending on Operating system
+            os.system('cls')
+        else:
+            os.system('clear')  # clear the console
+
         print_state(state)  # print the current state
         action = get_user_action()  # get the user action from the keyboard
         state = action_function(state, action)  # update the state with the new action
         if goal_test(initial_state_level2,goal_state_level2):  # check if the game is won
-            os.system('clear')
+            if o_system == 'Windows':  # Clearing depending on Operating system
+                os.system('cls')
+            else:
+                os.system('clear')
+
             print_state(state)
             print("You win!")
             break
